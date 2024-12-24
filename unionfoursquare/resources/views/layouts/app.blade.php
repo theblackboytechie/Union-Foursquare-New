5257c728c98a3cf71b871e5a5f440b575b6f63b5
theblackboytechie <157506948+theblackboytechie@users.noreply.github.com>
2024-12-24 15:56:18 +0100

    home page
    home page coding designs

--- unionfoursquare/resources/views/layouts/app.blade.php
+++ unionfoursquare/resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- fontawesome cdn -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
         <!-- Scripts -->
         @vite([
             'resources/css/app.css',
--- unionfoursquare/resources/views/publicpages/router.blade.php
+++ unionfoursquare/resources/views/publicpages/router.blade.php
@@ -5,12 +5,12 @@
     >
         <!-- navigation! -->
         <div class="header-navigation general-padding">
-            <div><img src="storage/assets/images/churchlogo.png" class="header-church-logo" alt=""></div>
+            <div><img src="/storage/assets/images/churchlogo.png" class="header-church-logo" alt=""></div>
             <div class="header-right-navigation">
                 <div>Home</div>
                 <div>About</div>
                 <div>Blog</div>
-                <div>Donations</div>
+                <div>Events</div>
                 <div>Contact</div>
                 <div class="header-button">Give Now</div>
             </div>
@@ -27,14 +27,252 @@
     <!-- the theme of foursquare! -->
     <div class="homepage-our-mandate">
         <!-- <div class="homepage-our-mandate-top">OUR MANDATE</div> -->
-        <div class="homepage-our-mandate-bottom"><u>Jesus Christ</u> the same yesterday, and today, and forever.</div>
+        <div class="homepage-our-mandate-bottom general-font-style"><u>Jesus Christ</u> the same yesterday, and today, and forever.</div>
         <div class="homepage-our-mandate-top">HEBREWS 13:8</div>
     </div>
 
     <!-- yo mehn! -->
-     <div class="homepage" style="padding: 10vh 20vh;display: grid;grid-template-columns: 1fr 1fr 1fr;gap: 2vw;">
-        <div style="background: green;">a</div>
-        <div>b</div>
-        <div>c</div>
-     </div>
+    <div class="homepage-threeaspects-each-wraps general-padding2">
+        <div class="homepage-threeaspects-each-wraps-inner">
+            <div>
+                <img 
+                    loading="lazy" 
+                    decoding="async" 
+                    width="100%" 
+                    src="/storage/assets/images/001post.jpg"
+                    class="" 
+                    alt=""
+                >
+            </div>
+            <div class="homepage-threeaspects">
+                <div class="homepage-threeaspects-maintext general-font-style">Our Community</div>
+                <div class="general-font-style homepage-threeaspects-maintext-fonts"><i class="fa-regular fa-circle-right"></i></div>
+            </div>
+        </div>
+        <div class="homepage-threeaspects-each-wraps-inner">
+            <div>
+                <img 
+                    loading="lazy" 
+                    decoding="async" 
+                    width="100%" 
+                    src="/storage/assets/images/002post.jpg"
+                    class="" 
+                    alt=""
+                >
+            </div>
+            <div class="homepage-threeaspects">
+                <div class="homepage-threeaspects-maintext general-font-style">Our Mission</div>
+                <div class="general-font-style homepage-threeaspects-maintext-fonts"><i class="fa-regular fa-circle-right"></i></div>
+            </div>
+        </div>
+        <div class="homepage-threeaspects-each-wraps-inner">
+            <div>
+                <img 
+                    loading="lazy" 
+                    decoding="async" 
+                    width="100%" 
+                    src="/storage/assets/images/003post.jpg"
+                    class="" 
+                    alt=""
+                >
+            </div>
+            <div class="homepage-threeaspects">
+                <div class="homepage-threeaspects-maintext general-font-style">Our Doctrine</div>
+                <div class="general-font-style homepage-threeaspects-maintext-fonts"><i class="fa-regular fa-circle-right"></i></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="general-padding2 general-font-style">
+        <div class="first-smaller-text">Why Union Foursquare?</div>
+        <div class="public-whyfoursquare-grid">
+            <div class="public-whyfoursquare-grid-left">We're a conservative Christian community that encourages you to come the way you are</div>
+            <div class="public-whyfoursquare-grid-right" style="font-weight: normal;">
+                <span class="public-whyfoursquare-grid-right-text">
+                    Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis consectetur adipiscing nostrud exercitation ullamco.
+                    <br><br>
+                    Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                </span>
+                <div style="margin-top: 10px;font-weight: bold;">View More <i class="fa-regular fa-circle-right"></i></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="public-three-arm-grid">
+        <div>
+            <div>
+                <img 
+                    loading="lazy" 
+                    decoding="async" 
+                    width="100%" 
+                    src="/storage/assets/images/three-grid-1.jpg"
+                    class="three-arm-image-style" 
+                    alt=""
+                >
+            </div>
+            <div class="public-three-arm-grid-each-text three-arm-under-border-right">
+                <div class="public-three-arm-grid-each-text-actual">Join Our Christian Community</div>
+            </div>
+            <div class="three-arm-under-border-right">
+                <div class="public-three-arm-grid-each-description">Upstream seeks out people that are impacting the world around them but lack the necessities to further their work.</div>
+            </div>
+            <div class="three-arm-under-border-right" style="padding-bottom: 100px;">
+                <button class="three-arm-button">Join</button>
+            </div>
+        </div>
+        <div>
+            <div>
+                <img 
+                    loading="lazy" 
+                    decoding="async" 
+                    width="100%" 
+                    src="/storage/assets/images/three-grid-2.jpg"
+                    class="three-arm-image-style" 
+                    alt=""
+                >
+            </div>
+            <div class="public-three-arm-grid-each-text three-arm-under-border-right">
+                <div class="public-three-arm-grid-each-text-actual">Join Our Christian Community</div>
+            </div>
+            <div class="three-arm-under-border-right">
+                <div class="public-three-arm-grid-each-description">Upstream seeks out people that are impacting the world around them but lack the necessities to further their work.</div>
+            </div>
+            <div class="three-arm-under-border-right" style="padding-bottom: 100px;">
+                <button class="three-arm-button">Join</button>
+            </div>
+        </div>
+        <div>
+            <div>
+                <img 
+                    loading="lazy" 
+                    decoding="async" 
+                    width="100%" 
+                    src="/storage/assets/images/three-grid-3.jpg"
+                    class="" 
+                    alt=""
+                >
+            </div>
+            <div class="public-three-arm-grid-each-text three-arm-under-border-right" style="border-right: 0px;">
+                <div class="public-three-arm-grid-each-text-actual">Join Our Christian Community</div>
+            </div>
+            <div class="three-arm-under-border-right" style="border-right: 0px;">
+                <div class="public-three-arm-grid-each-description">Upstream seeks out people that are impacting the world around them but lack the necessities to further their work.</div>
+            </div>
+            <div class="three-arm-under-border-right" style="padding-bottom: 100px;border-right: 0px;">
+                <button class="three-arm-button">Join</button>
+            </div>
+        </div>
+        <!-- <div>2</div>
+        <div>3</div> -->
+    </div>
+    <div class="public-our-doctrine-section general-padding2 general-font-style">
+        <div class="first-smaller-text">OUR BELIEF</div>
+        <div class="public-three-arm-grid-each-text">
+            <div class="homepage-our-mandate-bottom general-font-style">Loving God, helping others and serving the world</div>
+        </div>
+        <div class="public-ourbelief-wraps">
+            <div class="public-ourbelief-each">
+                <div class="public-ourbelief-flex-img">
+                    <img 
+                        loading="lazy" 
+                        decoding="async" 
+                        width="40%" 
+                        src="/storage/assets/images/animated/holybible.gif"
+                        class="" 
+                        alt=""
+                    >
+                </div>
+                <div class="public-maintext">Holy Scriptures</div>
+                <div class="public-description font-normal">tfgy fgvyuh jgyuh idftgy jfguhi fgyuh ftgyuh jfgvh ufjgyjh fgyvuh jgyh jdfgyh</div>
+            </div>
+            <div class="public-ourbelief-each">
+                <div class="public-ourbelief-flex-img">
+                    <img 
+                        loading="lazy" 
+                        decoding="async" 
+                        width="40%" 
+                        src="/storage/assets/images/animated/church.gif"
+                        class="" 
+                        alt=""
+                    >
+                </div>
+                <div class="public-maintext">Eternal Godhead</div>
+                <div class="public-description font-normal">tfgy fgvyuh jgyuh idftgy jfguhi fgyuh ftgyuh jfgvh ufjgyjh fgyvuh jgyh jdfgyh</div>
+            </div>
+            <div class="public-ourbelief-each">
+                <div class="public-ourbelief-flex-img">
+                    <img 
+                        loading="lazy" 
+                        decoding="async" 
+                        width="40%" 
+                        src="/storage/assets/images/animated/fallofman.gif"
+                        class="" 
+                        alt=""
+                    >
+                </div>
+                <div class="public-maintext">Fall of Man</div>
+                <div class="public-description font-normal">tfgy fgvyuh jgyuh idftgy jfguhi fgyuh ftgyuh jfgvh ufjgyjh fgyvuh jgyh jdfgyh</div>
+            </div>
+            <div class="public-ourbelief-each">
+                <div class="public-ourbelief-flex-img">
+                    <img 
+                        loading="lazy" 
+                        decoding="async" 
+                        width="40%" 
+                        src="/storage/assets/images/animated/plan-redemption.gif"
+                        class="" 
+                        alt=""
+                    >
+                </div>
+                <div class="public-maintext">Plan of Redemption</div>
+                <div class="public-description font-normal">tfgy fgvyuh jgyuh idftgy jfguhi fgyuh ftgyuh jfgvh ufjgyjh fgyvuh jgyh jdfgyh</div>
+            </div>
+        </div>
+        <div style="margin-top: 40px;">
+            <button class="header-button text-white">ALL OUR BELIEFS</button>
+        </div>
+    </div>
+
+    <!-- upcoming events -->
+    <div class="general-padding2">
+        <div class="first-smaller-text">CALENDAR</div>
+        <div class="homepage-our-mandate-bottom" style="text-align: left;">Event schedule</div>
+        <div>
+            <div class="public-upcoming-events-header">
+                <div>May 2021</div>
+                <div>June 2023</div>
+                <div>July 2023</div>
+            </div>
+            <div style="display: flex;flex-direction: column;gap: 10px;">
+                @for ($i = 2; $i <= 10; $i++)
+                    <div style="display: flex; border-bottom: 1px solid rgba(128, 128, 128, 0.2);padding: 20px 0px;">
+                        <div style="width: 15%;">
+                            <div style="font-weight: bolder;font-size: 20px;">1 July</div>
+                            <div style="font-weight: normal;">12:00 am</div>
+                        </div>
+                        <div style="width: 25%;font-size: 20px;font-weight: bolder;">Newcomers Lunch</div>
+                        <div style="width: 25%;">
+                            <!--  -->
+                            <div style="font-weight: bolder;font-size: 20px;">Rev. Lawrence Amajor</div>
+                            <div style="font-weight: normal;">Lead Pastor</div>
+                        </div>
+                        <div style="width: 20%;">
+                            <div style="font-weight: bolder;font-size: 20px;">Main Church Auditorium</div>
+                            <div style="font-weight: normal;">Elizabeth, Newwark New Jersey</div>
+                        </div>
+                        <div style="width: 15%;font-size: 20px;text-align: right;"><i class="fa-regular fa-circle-right"></i></div>
+                    </div>
+                @endfor
+            </div>
+        </div>
+    </div>
+
+    <!--  -->
+    <div class="" style="height: 400px;background: url('storage/assets/images/section-bckg.jpg'); background-position: center;background-repeat: no-repeat;background-size: cover; width: 100%;padding-bottom: 10vh;">
+
+    </div>
+
+    <div>
+        footer: <a href="https://lordicon.com/">Icons by Lordicon.com</a>
+    </div>
 </x-app-layout>
