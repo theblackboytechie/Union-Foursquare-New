d9e6036a34793ee050ac8178b3b3ea4742b84430
theblackboytechie <157506948+theblackboytechie@users.noreply.github.com>
2024-12-25 19:30:22 +0100

    index responsiveness
    responsive designs for home page

--- unionfoursquare/resources/views/publicpages/router.blade.php
+++ unionfoursquare/resources/views/publicpages/router.blade.php
@@ -7,17 +7,18 @@
         <div class="header-navigation general-padding">
             <div><img src="/storage/assets/images/churchlogo.png" class="header-church-logo" alt=""></div>
             <div class="header-right-navigation">
-                <div>Home</div>
-                <div>About</div>
-                <div>Blog</div>
-                <div>Events</div>
-                <div>Contact</div>
-                <div class="header-button">Give Now</div>
+                <div class="remove-for-mobile">Home</div>
+                <div class="remove-for-mobile">About</div>
+                <div class="remove-for-mobile">Blog</div>
+                <div class="remove-for-mobile">Events</div>
+                <div class="remove-for-mobile">Contact</div>
+                <div class="header-button remove-for-mobile">Give Now</div>
+                <div class="header-mobilemenu-icon"><i class="fa-solid fa-bars"></i></div>
             </div>
         </div>
         <div class="header-homepage-banner-content general-padding">
             <div class="header-homepage-banner-content-bottom">Terbanacle of His Presence</div>
-            <div class="header-homepage-banner-content-description">gy ugj ftygu ftgyuh tfgvh jdrtfc tfgyuh igyu fjtgyu drf udtfgyu tfgy uftgyu ftyu ftgyvu ftgvyu ftgyuh ifrgyh fyuguh jrdcgy gyuhi ftgyvhgf tyfg drtfy dutrf yudtrf udtfgy tfgyuhjk</div>
+            <div class="header-homepage-banner-content-description" style="font-family: 'Poppins', serif;font-weight: 100;font-size: 25px;">Welcome to a community of God's people, guided by scriptures and the pure love of God</div>
             <div style="display: flex; justify-content: center;gap: 1vw;">
                 <div class="header-button text-white">Service Times</div>
                 <div class="header-button text-white">Service Times</div>
@@ -26,12 +27,11 @@
     </div>
     <!-- the theme of foursquare! -->
     <div class="homepage-our-mandate">
-        <!-- <div class="homepage-our-mandate-top">OUR MANDATE</div> -->
         <div class="homepage-our-mandate-bottom general-font-style"><u>Jesus Christ</u> the same yesterday, and today, and forever.</div>
-        <div class="homepage-our-mandate-top">HEBREWS 13:8</div>
+        <!-- <div class="homepage-our-mandate-top">HEBREWS 13:8</div> -->
     </div>
 
-    <!-- yo mehn! -->
+    <!--  -->
     <div class="homepage-threeaspects-each-wraps general-padding2">
         <div class="homepage-threeaspects-each-wraps-inner">
             <div>
@@ -83,6 +83,7 @@
         </div>
     </div>
 
+    <!--  -->
     <div class="general-padding2 general-font-style">
         <div class="first-smaller-text">Why Union Foursquare?</div>
         <div class="public-whyfoursquare-grid">
@@ -96,295 +97,5 @@
                 <div style="margin-top: 10px;font-weight: bold;">View More <i class="fa-regular fa-circle-right"></i></div>
             </div>
         </div>
-    </div>
-
-    <div class="public-three-arm-grid">
-        <div>
-            <div>
-                <img 
-                    loading="lazy" 
-                    decoding="async" 
-                    width="100%" 
-                    src="/storage/assets/images/three-grid-1.jpg"
-                    class="three-arm-image-style" 
-                    alt=""
-                >
-            </div>
-            <div class="public-three-arm-grid-each-text three-arm-under-border-right">
-                <div class="public-three-arm-grid-each-text-actual">Join Our Christian Community</div>
-            </div>
-            <div class="three-arm-under-border-right">
-                <div class="public-three-arm-grid-each-description">Upstream seeks out people that are impacting the world around them but lack the necessities to further their work.</div>
-            </div>
-            <div class="three-arm-under-border-right" style="padding-bottom: 100px;">
-                <button class="three-arm-button">Join</button>
-            </div>
-        </div>
-        <div>
-            <div>
-                <img 
-                    loading="lazy" 
-                    decoding="async" 
-                    width="100%" 
-                    src="/storage/assets/images/three-grid-2.jpg"
-                    class="three-arm-image-style" 
-                    alt=""
-                >
-            </div>
-            <div class="public-three-arm-grid-each-text three-arm-under-border-right">
-                <div class="public-three-arm-grid-each-text-actual">Join Our Christian Community</div>
-            </div>
-            <div class="three-arm-under-border-right">
-                <div class="public-three-arm-grid-each-description">Upstream seeks out people that are impacting the world around them but lack the necessities to further their work.</div>
-            </div>
-            <div class="three-arm-under-border-right" style="padding-bottom: 100px;">
-                <button class="three-arm-button">Join</button>
-            </div>
-        </div>
-        <div>
-            <div>
-                <img 
-                    loading="lazy" 
-                    decoding="async" 
-                    width="100%" 
-                    src="/storage/assets/images/three-grid-3.jpg"
-                    class="" 
-                    alt=""
-                >
-            </div>
-            <div class="public-three-arm-grid-each-text three-arm-under-border-right" style="border-right: 0px;">
-                <div class="public-three-arm-grid-each-text-actual">Join Our Christian Community</div>
-            </div>
-            <div class="three-arm-under-border-right" style="border-right: 0px;">
-                <div class="public-three-arm-grid-each-description">Upstream seeks out people that are impacting the world around them but lack the necessities to further their work.</div>
-            </div>
-            <div class="three-arm-under-border-right" style="padding-bottom: 100px;border-right: 0px;">
-                <button class="three-arm-button">Join</button>
-            </div>
-        </div>
-        <!-- <div>2</div>
-        <div>3</div> -->
-    </div>
-    <div class="public-our-doctrine-section general-padding2 general-font-style">
-        <div class="first-smaller-text">OUR BELIEF</div>
-        <div class="public-three-arm-grid-each-text">
-            <div class="homepage-our-mandate-bottom general-font-style">Loving God, helping others and serving the world</div>
-        </div>
-        <div class="public-ourbelief-wraps">
-            <div class="public-ourbelief-each">
-                <div class="public-ourbelief-flex-img">
-                    <img 
-                        loading="lazy" 
-                        decoding="async" 
-                        width="40%" 
-                        src="/storage/assets/images/animated/holybible.gif"
-                        class="" 
-                        alt=""
-                    >
-                </div>
-                <div class="public-maintext">Holy Scriptures</div>
-                <div class="public-description font-normal">tfgy fgvyuh jgyuh idftgy jfguhi fgyuh ftgyuh jfgvh ufjgyjh fgyvuh jgyh jdfgyh</div>
-            </div>
-            <div class="public-ourbelief-each">
-                <div class="public-ourbelief-flex-img">
-                    <img 
-                        loading="lazy" 
-                        decoding="async" 
-                        width="40%" 
-                        src="/storage/assets/images/animated/church.gif"
-                        class="" 
-                        alt=""
-                    >
-                </div>
-                <div class="public-maintext">Eternal Godhead</div>
-                <div class="public-description font-normal">tfgy fgvyuh jgyuh idftgy jfguhi fgyuh ftgyuh jfgvh ufjgyjh fgyvuh jgyh jdfgyh</div>
-            </div>
-            <div class="public-ourbelief-each">
-                <div class="public-ourbelief-flex-img">
-                    <img 
-                        loading="lazy" 
-                        decoding="async" 
-                        width="40%" 
-                        src="/storage/assets/images/animated/fallofman.gif"
-                        class="" 
-                        alt=""
-                    >
-                </div>
-                <div class="public-maintext">Fall of Man</div>
-                <div class="public-description font-normal">tfgy fgvyuh jgyuh idftgy jfguhi fgyuh ftgyuh jfgvh ufjgyjh fgyvuh jgyh jdfgyh</div>
-            </div>
-            <div class="public-ourbelief-each">
-                <div class="public-ourbelief-flex-img">
-                    <img 
-                        loading="lazy" 
-                        decoding="async" 
-                        width="40%" 
-                        src="/storage/assets/images/animated/plan-redemption.gif"
-                        class="" 
-                        alt=""
-                    >
-                </div>
-                <div class="public-maintext">Plan of Redemption</div>
-                <div class="public-description font-normal">tfgy fgvyuh jgyuh idftgy jfguhi fgyuh ftgyuh jfgvh ufjgyjh fgyvuh jgyh jdfgyh</div>
-            </div>
-        </div>
-        <div style="margin-top: 40px;">
-            <button class="header-button text-white">ALL OUR BELIEFS</button>
-        </div>
-    </div>
-
-    <!-- upcoming events -->
-    <div class="general-padding2">
-        <div class="first-smaller-text">CALENDAR</div>
-        <div class="homepage-our-mandate-bottom" style="text-align: left;">Event schedule</div>
-        <div>
-            <div class="public-upcoming-events-header">
-                <div>May 2021</div>
-                <div>June 2023</div>
-                <div>July 2023</div>
-            </div>
-            <div class="public-upcomingevents-list">
-                @for ($i = 2; $i <= 10; $i++)
-                    <div class="public-upcomingevents-list-inner">
-                        <div style="width: 15%;">
-                            <div class="public-upcomingevents-date">1 July</div>
-                            <div class="public-upcomingevents-time">12:00 am</div>
-                        </div>
-                        <div class="public-upcomingevents-eventname">Newcomers Lunch</div>
-                        <div style="width: 25%;">
-                            <!--  -->
-                            <div class="public-upcomingevents-hostname">Rev. Lawrence Amajor</div>
-                            <div style="font-weight: normal;">Lead Pastor</div>
-                        </div>
-                        <div style="width: 20%;">
-                            <div class="public-upcomingevents-event-location">Main Church Auditorium</div>
-                            <div class="public-upcomingevents-event-address">Elizabeth, Newwark New Jersey</div>
-                        </div>
-                        <div class="public-upcomingevents-icon-wrap"><i class="fa-regular fa-circle-right"></i></div>
-                    </div>
-                @endfor
-            </div>
-        </div>
-    </div>
-
-    <!--  -->
-    <div class="general-padding2" style="background: url('storage/assets/images/section-bckg.jpg'); background-position: center;background-repeat: no-repeat;background-size: cover; width: 100%;padding-bottom: 10vh;">
-        <div class="first-smaller-text">LATEST SERMON</div>
-        <div style="display: grid;grid-template-columns: 70% 30%;align-items: center;">
-            <div class="homepage-audioplayer-title">Word, Worship and Wonder</div>
-            <div style="display: flex;align-items: center;justify-content: flex-end;gap: 10px;">More Audio <i class="fa-regular fa-circle-right"></i></div>
-        </div>
-            <div style="display: grid;grid-template-columns: 25% 75%;margin-top: 20px;">
-                <div>
-                    <img 
-                        loading="lazy" 
-                        decoding="async" 
-                        width="100%" 
-                        src="/storage/assets/images/audio-message.jpg"
-                        class="" 
-                        alt=""
-                    >
-                </div>
-                <div style="display: flex;align-items: center;justify-content: center;background: white;">
-                    <div style="width: 80%;">
-                        <!--  -->
-                        <div>
-                            <button style="background: rgba(36, 12, 0, 1);margin-bottom: 10px;padding: 5px 20px;color: white;font-size: 12px;">NOW PLAYING</button>
-                        </div>
-                        <div class="general-font-style" style="margin-bottom: 10px;font-size: 35px;">Serve the world: come together</div>
-                        <audio controls loop preload="auto" style="background: rgba(0,0,0,0.2);width: 100%;">
-                            <source src="audio_file.mp3" type="audio/mp3">
-                            Your browser does not support the audio element.
-                        </audio>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- our blog -->
-    <div class="general-padding2">
-        <!-- this is higga! -->
-        <div class="first-smaller-text">READ OUR BLOG</div>
-        <div style="display: grid;grid-template-columns: 70% 30%;align-items: center;">
-            <div class="homepage-audioplayer-title">Latest Articles</div>
-            <div style="display: flex;align-items: center;justify-content: flex-end;gap: 10px;">More Posts <i class="fa-regular fa-circle-right"></i></div>
-        </div>
-        <div style="display: grid;grid-template-columns: 1fr 1fr;gap: 50px;margin-top: 20px;">
-            <div>
-                <div>
-                    <img 
-                        loading="lazy" 
-                        decoding="async" 
-                        width="100%" 
-                        src="/storage/assets/images/001post.jpg"
-                        class="" 
-                        alt=""
-                    >
-                </div>
-                <div style="display: grid;grid-template-columns: 15% 75%;align-items: start;gap: 5%;margin-top: 40px;">
-                    <div>
-                        <div style="font-size: 50px;font-weight: bolder;text-align: center;">06</div>
-                        <div style="text-align: center;">APR</div>
-                    </div>
-                    <div class="general-font-style" style="font-size: 40px;line-height: 35px;">Love for our Lord is pure and unconditional</div>
-                </div>
-                <div style="display: grid;grid-template-columns: 15% 78%;gap: 5%;margin-top: 10px;font-weight: normal;">
-                    <div></div>
-                    <div>
-                        <!--  -->
-                        <div style="color:rgba(36, 12, 0, 0.8);">
-                            hv utrgu tfty uftyui fvyuh ifkgyuh jgkyuhj ifkyvuh kgyuh fyvuh
-                            fyvuhji kghj fcgyhj fvhj kfvuhjh fkgvhi fjghjd tfcg fhj tygf
-                        </div>
-                        <div style="display: flex;align-items: center;gap: 10px;margin-top: 10px;color:rgba(36, 12, 0, 0.8);">
-                            <div>0 Likes</div>
-                            <div>0 Comments</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div>
-                @for ($i = 0; $i <= 2; $i++)
-                    <div style="display: grid;grid-template-columns: 30% 65%;align-items: center;gap: 5%;margin-bottom: 20px;">
-                        <div>
-                            <!--  -->
-                            <img 
-                                loading="lazy" 
-                                decoding="async" 
-                                width="100%" 
-                                src="/storage/assets/images/blogitem.jpg"
-                                class="" 
-                                alt=""
-                            >
-                        </div>
-                        <div>
-                            <div style="display: flex;align-items: center;gap: 5px;font-weight: normal;">
-                                <div>Rev. Lawrence Amajor</div>
-                                <div style="color:rgba(36, 12, 0, 0.4);font-size: 5px;"><i class="fa-solid fa-circle-dot"></i></div>
-                                <div>3rd Dec., 2023</div>
-                            </div>
-                            <div class="general-font-style" style="font-size: 25px;line-height: 28px;">We bring heaven on earth through Christ</div>
-                            <div style="display: flex;align-items: center;gap: 10px;margin-top: 10px;color:rgba(36, 12, 0, 0.8);">
-                                <div>0 Likes</div>
-                                <div>0 Comments</div>
-                            </div>
-                        </div>
-                    </div>
-                @endfor
-            </div>
-        </div>
-    </div>
-
-    <div class="general-padding2" style="height: 400px;background: url('storage/assets/images/section-bckg.jpg'); background-position: center;background-repeat: no-repeat;background-size: cover; width: 100%;padding-bottom: 10vh;display: flex;flex-direction: column;align-items: center;justify-content: center;">
-        <div style="display: flex;justify-content: center;">
-            <!--  -->
-            <i class="fa-brands fa-telegram"></i>
-        </div>
-        <div>vvv</div>
-    </div>
-
-    <div>
-        footer: <a href="https://lordicon.com/">Icons by Lordicon.com</a>
     </div>
 </x-app-layout>
