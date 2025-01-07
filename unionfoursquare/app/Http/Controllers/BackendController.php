0e1c7ae41e4625934088b1f93b4b4a9ca695d03d
theblackboytechie <157506948+theblackboytechie@users.noreply.github.com>
2025-01-07 10:45:44 +0100

    Update BackendController.php
    Update BackendController.php for jumbotron updates

--- unionfoursquare/app/Http/Controllers/BackendController.php
+++ unionfoursquare/app/Http/Controllers/BackendController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Helpers\CrudHelper;
 use Illuminate\Http\Request;
 
 class BackendController extends Controller
@@ -45,6 +45,28 @@ class BackendController extends Controller
 
     public function database_update(Request $request)
     {
-        return "Jack&Rose!";
+        // return "Jack&Rose!";
+        if($request->owner == "jumbotron-update"){
+            $tabledb = "pagesui";
+
+            $where_array = [
+                'owner_id' => $request->ownerid,
+                'headline' => $request->headline,
+                'subtext' => $request->subtext,
+                'text1' => $request->text1,
+                'link1' => $request->link1,
+                'text2' => $request->text2,
+                'link2' => $request->link2,
+            ];
+    
+            $output = CrudHelper::Get($tabledb, $where_array);
+        }
+        // $owner => $request->owner;
+        // $headline => $request->headline;
+        // subtext: subtext,
+        // text1: text1,
+        // link1: link1,
+        // text2: text2,
+        // link2: link2
     }
 }
